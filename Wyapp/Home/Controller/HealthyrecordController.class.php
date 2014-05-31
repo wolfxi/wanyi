be6401c7deb23f87db44279c5dfe41f5f9ff854b
wolfxi <1150665265@qq.com>
2014-05-31 22:48:58 +0800

    添加疾病/用药/检查   php代码基本完成，没插入数据库，需要修改表结构

--- Wyapp/Home/Controller/HealthyrecordController.class.php
+++ Wyapp/Home/Controller/HealthyrecordController.class.php
@@ -155,7 +155,7 @@ class HealthyrecordController extends HomeController{
 
 	//存取疾病确诊时间
 	//return json
-	public function getDateD(){
+	public function getDate(){
 	
 		if(IS_AJAX && !empty(I('post.date')) && !empty(I('post.uniqid'))){
 
@@ -287,35 +287,315 @@ class HealthyrecordController extends HomeController{
 
 	/***=========================添加用药 M   start===================**/
 	
+	//添加用药界面
+	//显示界面
+	public function addMedicle(){
+	
+		$this->display();
+	
+	}
+	
+	//拼音快速查找用药
+	//从字典中获取数据
+	//return json 二维数组
+	public function quickFindMedicle(){
+		if(IS_AJAX && empty(I('post.medicle'))){
+			$search_str=I('post.desease');	
+			$result=$this->DictionaryApi->PinYinSearch('medicinedictionary','md_name_pinying',$search_str);	
+			$res=array();
+			if($result && is_array($result)){
+				$res['flag']=true;
+				$res['msg']=$result;
+			}else{
+				$res['flag']=true;
+				$res['msg']='';
+			}
+			$this->ajaxReturn($res);
+		}else{
+			die();
+		}
+
+	}
+	
+		
+	
+	//选定用药
+	//将用药存在session中 session的ID可以发送到界面
+	//返回的时候要携带该ID来获取存在session中的数据
+	//该session中存的是一个数组
+	public function choseConfirmMedicle(){
+
+		if(IS_AJAX && !empty(I('post.mname'))){
+			$dname=(string)I('post.mname');
+			$result=array();
+			if($this->DictionaryApi->existSelect('medicinedictionary','md_name',$mname)){
+				$temp_id=uniqid();
+				$temp_array['mname']=$mname;
+				//存如一个session中
+				session($temp_id,$temp_array);
+				$result['uniqid']=$temp_id;
+				$result['flag']=true;
+				$result['msg']='OK';
+
+			}else{
+				$result['flag']=false;
+				$result['msg']='没有该药';
+			}
+			$this->ajaxReturn($result);
+		}else{
+			die();
+		}
+	} 
+
+
+	//获取用药时间界面
+	//@param mname :药名
+	//return html
+	public function getDateMHtml(){
+		if(IS_AJAX && !empty(I('post.mname'))){
+			$this->assign('mname',I('post.mname'));
+			$this->assign('flag',true);
+			$this->display();
+		
+		}else{
+		die();
+		}	
+	
+	}
 	
 	
+	//获取用药有没有效果界面
+	//@param mname
+	//return html
+	public function getEffectMhtml(){
+
+		if(IS_AJAX && !empty(I('post.mname')) ){
+			$mname=I('post.mname');
+			$this->assign('mname',$mname);
+			$this->assign('flag',true);
+			$this->display();
+
+		}else{
+			die();
+		}
+
+	}
+
 	
+	//获取用药是否有效
+	//@param meffect yes/no
+	//@param uniqid session索引
+	//return json
+	public function getEffectM(){
+
+		if(IS_AJAX && !empty(I('post.meffect')) && !empty(I('post.uniqid'))){
+			$uniqid=I('post.uniqid');
+			$result=array();
+			if(session('?'.$uniqid){
+					$temp_array=session($uniqid);
+					$meffect=I('post.meffect');
+					$effect=0;
+					switch($meffect){
+	 					case 1 : 
+							$effect='有效';
+							break;
+						case 2 :
+							$effect='无效';
+							break;
+						//TODO:: 根据具体情况
+						default:
+							$effect=0;
+					}
+					$result['flag']=true;
+					$result['msg']='OK';
+					$this->ajaxReturn($result);
+			}else{
+				$result['falg']=false;
+				$result['msg']='没有存在信息';
+				$this->ajaxReturn($result);
+			}
+		}else{
+			die();
+		}
+	}
+
+
+
+	//获取用药有没有副作用界面
+	//@param mname
+	//return html
+	public function getFeffectMhtml(){
+
+		if(IS_AJAX && !empty(I('post.mname')) ){
+			$mname=I('post.mname');
+			$this->assign('mname',$mname);
+			$this->assign('flag',true);
+			$this->display();
+
+		}else{
+			die();
+		}
+
+	}
+
+
+	//获取用药是否有副作用
+	//@param mfeffect yes/no
+	//@param mfeffectstr  副作用的说明
+	//@param uniqid session索引
+	//return json
+	public function getFeffectM(){
+
+		if(IS_AJAX && !empty(I('post.mfeffect'))  && !empty(I('post.mfeffectstr')) && !empty(I('post.uniqid'))){
+			$uniqid=I('post.uniqid');
+			$result=array();
+			if(session('?'.$uniqid){
+					$temp_array=session($uniqid);
+					$temp_array['mfeffect']=I('post.meffect');
+					$temp_array['mfeffectstr']=I('post.mfeffectstr');
+
+					$result['flag']=true;
+					$result['msg']='OK';
+					$this->ajaxReturn($result);
+			}else{
+				$result['falg']=false;
+				$result['msg']='没有存在信息';
+				$this->ajaxReturn($result);
+			}
+		}else{
+			die();
+		}
+	}
+
+	//获取完成界面的按钮
+	//return html 
+	public function getDonMHtml(){
 	
+		if(IS_AJAX){
+		
+			$this->assign('flag',true);
+			$this->display();
+		}else{
+		die();
+		}
 	
+	}
+
+	//添加用药完成按钮操作
+	//return json
+	public function addDoneM(){
+		if(IS_AJAX && !empty(I('post.uniqid'))){
+			$uniqid=I('post.uniqid');
+			$temp_array=session($uniqid);
+			//TODO::需要修改表结构在做
+		
+		}else{
+		die();
+		}
 	
-	
-	
-	
-	
-	
-	
-	
-	
+	}
+
 	
 	
 	
 	
 	/***=========================添加用药 end===================**/
 
-	///////////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////// 
+	/***=========================添加检查 C start===================**/
+	
+	
+	//检查界面
+	public function addCheck(){
+	
+		$this->display();
+	
+	}
+	
+	//拼音快速查找检查
+	//根据检查字典获取数据
+	//return 二维数组
+	public function quickFindCheck(){
+		if(IS_AJAX && empty(I('post.check'))){
+			$search_str=I('post.check');	
+			$result=$this->DictionaryApi->PinYinSearch('checksurgeytreatment','cst_pinying',$search_str);	
+			$res=array();
+			if($result && is_array($result)){
+				$res['flag']=true;
+				$res['msg']=$result;
+			}else{
+				$res['flag']=true;
+				$res['msg']='';
+			}
+			$this->ajaxReturn($res);
+		}else{
+			die();
+		}
+	
+	}
 
-	/***=========================添加检查 start===================**/
+
+	//选定检查项目
+	//将检查项目放在session中 session的ID可以发送到界面
+	//返回的时候要携带该ID来获取存在session中的数据
+	//该session中存的是一个数组
+	public function choseConfirmCheck(){
+
+		if(IS_AJAX && !empty(I('post.cname'))){
+			$cname=(string)I('post.cname');
+			$result=array();
+			if($this->DictionaryApi->existSelect('checksurgeytreatment','cst_name',$cname)){
+				$temp_id=uniqid();
+				$temp_array['cname']=$cname;
+				//存如一个session中
+				session($temp_id,$temp_array);
+				$result['uniqid']=$temp_id;
+				$result['flag']=true;
+				$result['msg']='OK';
+
+			}else{
+				$result['flag']=false;
+				$result['msg']='没有该检查';
+			}
+			$this->ajaxReturn($result);
+		}else{
+			die();
+		}
+	} 
+
+
+	//获取检查值界面
+	//return html
+	public function getCheckValueHtml(){
+		if(IS_AJAX && !empty(I('post.cname'))){
+			$this->assign('flag',true);
+			$this->assign('cname',I('post.cname'));
+
+			$this->display();
+		
+		}else{
+			die();
+		}
 	
 	
+	}
+
+
+	//完成按钮操作
+	//return json
+	public function addDoneCheck(){
 	
-	
-	
-	
+		if(IS_AJAX && !empty(I('post.uniqid')) && !empty(I('post.cvalue'))){
+		
+			$uniqid=I('post.uniqid');
+			$cvalue=I('post.cvalue');
+
+			$temp_array=session($uniqid);
+			//TODO::修改好表后在再做
+		}else{
+			die();
+		}
+	}
 	
 	
 	
